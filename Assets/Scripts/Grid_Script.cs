933b60120af2128fb755262289886a9b03f734b6
Herbert <09tunafish1285@gmail.com>
2023-10-26 16:59:00 +0100

    Update Grid_Script.cs

--- Assets/Scripts/Grid_Script.cs
+++ Assets/Scripts/Grid_Script.cs
@@ -5,25 +5,23 @@ using UnityEngine;
 public class Grid_Script : MonoBehaviour
 {
 
-    public int gridSize = 11;
+    public int gridSize = 11; //initial grid size
     public GameObject squarePrefab;
 
-    //get screen height
+    private int direction = 1; //0 = up, 1 = right, 2 = down, 3 = left
 
-    //new 2d array to hold square coords
-
-
-    // Start is called before the first frame update
     void Start()
     {
         int incriment = 0;
         float stepSize = 10f / gridSize;
         float[,] gridArr = new float[(gridSize * gridSize), 2];
 
+
+
         //change scale of grid
         squarePrefab.transform.localScale = new Vector3(stepSize, stepSize, stepSize);
 
-
+        //generate grid
         for (float x = 0 - (gridSize / 2) + 1; x < gridSize/2; x++)
         {
             for (float y = 0 - (gridSize / 2) + 1; y < gridSize/2; y++)
@@ -34,7 +32,8 @@ public class Grid_Script : MonoBehaviour
 
                 //create cube from prefab
                 GameObject square = Instantiate(squarePrefab, new Vector3(xAdjust, yAdjust, 0), Quaternion.identity);
-                //make every other cube black
+
+                //alternate colours
                 if (incriment % 2 == 0)
                 {
                     square.GetComponent<Renderer>().material.color = new Color(31/255f, 161/255f, 36/255f);
@@ -48,23 +47,28 @@ public class Grid_Script : MonoBehaviour
                 gridArr[incriment, 0] = xAdjust;
                 gridArr[incriment, 1] = yAdjust;
                 
-   
+                //save cube to array
+                square.name = "Cube " + incriment;
 
 
-
-                incriment++;
+                incriment++; //next cube
             }
         }
-        print(gridArr[2, 1]);
-        
-
 
 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //change random square colour to red
+        for (int i = 0; i < (gridSize * gridSize); i++)
+        {
+            int r = Random.Range(0, 255);
+            int g = Random.Range(0, 255);
+            int b = Random.Range(0, 255);
+            GameObject.Find("Cube " + i).GetComponent<Renderer>().material.color = new Color(r/255f, g/255f, b/255f);
+        }
+
 
     }
 }
